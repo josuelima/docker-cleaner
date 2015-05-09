0808cfe866057f39fffec499b04e0b8e5e1b39e9
Josué Lima <josuedsi@gmail.com>
2015-05-09 16:48:20 -0300

    Add clean containers

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # DOCKER CLEANER
-# Keep your docker environment clean by removing all unused docker containers and images
+# Keep your environment clean by removing all obsolete Docker images and containers
 #
 # For more information visit: github.com/josuelima/docker-cleaner
 #
@@ -11,18 +11,40 @@ import sys
 from docker import Client
 
 client = Client(base_url='unix://var/run/docker.sock')
-containers = client.containers(all = True)
 
-def should_delete_container(status):
+def should_remove_container(status):
   """
-  Given that we want to remove all obsolet containers (not running)
-  return True to all statuses not containing Up
+  Given that we want to remove all obsolet containers (=not running)
+  return True to all container statuses not containing Up
   """
   return not status.startswith('Up')
 
 def remove_container(container):
   """ Remove container given the container Id"""
-  containers.remove_container(container = container['Id'])
+  #containers.remove_container(container = container['Id'])
+  print "remove this"
 
-for container in containers:
-  if should_delete(container['Status']): remove_container(container)
+def should_remove_image(image, containers):
+  """
+  Determine if the image should be removed if there is no containers based on that
+  image running at the momment
+  """
+  for container in containers:
+    if container['Image'] in image['RepoTags']: return False
+
+  return True
+
+def clear_containers():
+  """ Search for all containers to remove those not running"""
+  for container in client.containers(all = True):
+    if should_remove_container(container['Status']): remove_container(container)
+
+def clear_images():
+  """
+  Search for images to remove those which
+  doesn't have containers instances running
+  """
+  running_containers = client.containers()
+
+  for image in client.images():
+    if should_delete_image(image, running_containers): remove_image(image)
