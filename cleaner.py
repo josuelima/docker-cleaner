2a940922719c805d10cac7bdaaed9ffa6ad871a8
Josué Lima <josuedsi@gmail.com>
2015-05-12 01:24:25 -0300

    Refactoring to use class

--- cleaner.py
+++ cleaner.py
@@ -11,70 +11,76 @@ import sys
 import yaml
 from docker import Client
 
-def should_remove_container(status):
-  """
-  Given that we want to remove all obsolet containers (=not running)
-  return True to all container statuses not containing 'Up'
-  """
-  return not status.startswith('Up')
+class Cleaner:
 
-def remove_container(container):
-  """ Try to remove container given the container Id """
-  try:
-    client.remove_container(container = container['Id'])
-    log("Removing container: %s" % container['Id'])
-  except:
-    log_error("container", container)
+  def __init__(self, configs):
+    self.configs = configs
+    self.client  = Client(base_url = self.configs['docker_endpoint'])
 
-def should_remove_image(image, containers):
-  """
-  Determine if the image should be removed if there is no containers based on that
-  image running at the momment
-  """
-  for container in containers:
-    if container['Image'] in image['RepoTags']: return False
+    """ Check the connection with docker. End execution if there's no connection """
+    try:
+      self.client.ping()
+    except:
+      self.log("Could not connect to Docker. Verify if docker is running or your settings file")
+      return
 
-  return True
+    if self.configs['clear_containers']: self.clear_containers()
+    if self.configs['clear_images']: self.clear_images()
 
-def remove_image(image):
-  """ Try to remove image given the image Id """
-  try:
-    client.remove_image(image = image['Id'], force = True)
-    log("Removing image: %s" % image['Id'])
-  except:
-    log_error("image", image)
+  def should_remove_container(self, status):
+    """
+    Given that we want to remove all obsolet containers (=not running)
+    return True to all container statuses not containing 'Up'
+    """
+    return not status.startswith('Up')
 
-def clear_containers():
-  """ Search for all containers in order to remove those not running """
-  for container in client.containers(all = True):
-    if should_remove_container(container['Status']): remove_container(container)
+  def remove_container(self, container):
+    """ Try to remove container given the container Id """
+    try:
+      self.client.remove_container(container = container['Id'])
+      self.log("Removing container: %s" % container['Id'])
+    except:
+      self.log_error("container", container)
 
-def clear_images():
-  """
-  Search for images in order to remove those which
-  doesn't have containers instances running
-  """
-  running_containers = client.containers()
+  def should_remove_image(self, image, containers):
+    """
+    Determine if the image should be removed if there is no containers based on that
+    image running at the momment
+    """
+    for container in containers:
+      if container['Image'] in image['RepoTags']: return False
 
-  for image in client.images():
-    if should_remove_image(image, running_containers): remove_image(image)
+    return True
 
-def log(message):
-  print message
+  def remove_image(self, image):
+    """ Try to remove image given the image Id """
+    try:
+      self.client.remove_image(image = image['Id'], force = True)
+      self.log("Removing image: %s" % image['Id'])
+    except:
+      self.log_error("image", image)
 
-def log_error(kind, instance):
-  log("Could not remove %s %s." % (kind, instance['Id']))
+  def clear_containers(self):
+    """ Search for all containers in order to remove those not running """
+    for container in self.client.containers(all = True):
+      if self.should_remove_container(container['Status']): self.remove_container(container)
+
+  def clear_images(self):
+    """
+    Search for images in order to remove those which
+    doesn't have containers instances running
+    """
+    running_containers = self.client.containers()
+
+    for image in self.client.images():
+      if self.should_remove_image(image, running_containers): self.remove_image(image)
+
+  def log(self, message):
+    print message
+
+  def log_error(self, kind, instance):
+    self.log("Could not remove %s %s." % (kind, instance['Id']))
 
 if __name__ == '__main__':
   configs = yaml.load(open('settings.yml', 'r'))
-
-  """ Try to connect to docker endpoint (socket/port) """
-  try:
-    client = Client(base_url = configs['docker_endpoint'])
-    client.ping()
-  except:
-    print "Could not connect to Docker. Verify if docker is running or your configs.yml file"
-    sys.exit()
-
-  if configs['clear_containers']: clear_containers()
-  if configs['clear_images']: clear_images()
+  Cleaner(configs)
